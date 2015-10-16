3b0d8101ecd4ff848d9e412ef3d09f004bb42984
Frank Xu <frank.xu@northbright.com>
2015-10-16 14:47:08 +0800

    1. Remove JwtHelper and use global KeyManager to manage keys with kid. 2. JwtHelper's functions are replaced with global functions. 3. Add ParseFromRequest() to parse token string in http request.

--- jwthelper.go
+++ jwthelper.go
@@ -7,8 +7,7 @@ import (
 	"github.com/northbright/errorhelper"
 	"github.com/northbright/pathhelper"
 	"io/ioutil"
-	"path"
-	"path/filepath"
+	"net/http"
 	"sync"
 )
 
@@ -19,16 +18,17 @@ type Key struct {
 	VerifyKey interface{}       // Verifying key. HMAC: []byte, RSA: *crypto/rsa.PublicKey, ECDSA: *crypto/ecdsa.PublicKey.
 }
 
-// JwtHelper manages the keys by using kid(key id).
-type JwtHelper struct {
+//  KeyManger manages the keys by using kid(key id).
+type KeyManager struct {
 	Keys         map[string]*Key // Key map. Key: kid(key id), Value: Key Struct
 	sync.RWMutex                 // Access map concurrently.
 }
 
-func New() (h *JwtHelper) {
-	return &JwtHelper{}
-}
+var (
+	km KeyManager = KeyManager{} // Internal key manager.
+)
 
+// ReadKey() reads key bytes from the key file.
 func ReadKey(keyFile string) (key []byte, err error) {
 	if err := errorhelper.GenEmptyStringError(keyFile, "keyFile"); err != nil {
 		return nil, err
@@ -36,14 +36,8 @@ func ReadKey(keyFile string) (key []byte, err error) {
 
 	// Make Abs key file path with current executable path if KeyFilePath is relative.
 	p := ""
-	if !filepath.IsAbs(keyFile) {
-		dir := ""
-		if dir, err = pathhelper.GetCurrentExecDir(); err != nil {
-			return nil, err
-		}
-		p = path.Join(dir, keyFile)
-	} else {
-		p = keyFile
+	if p, err = pathhelper.GetAbsPath(keyFile); err != nil {
+		return nil, err
 	}
 
 	buf := []byte{}
@@ -54,20 +48,26 @@ func ReadKey(keyFile string) (key []byte, err error) {
 	return buf, nil
 }
 
-func (h *JwtHelper) setKey(kid string, key *Key) {
-	h.Lock()
-	h.Keys[kid] = key
-	h.Unlock()
+// setKey() sets the kid - Key pair.
+//
+//   Params:
+//       kid: Key id. It should be unique.
+//       key: Key struct.
+func setKey(kid string, key *Key) {
+	km.Lock()
+	km.Keys[kid] = key
+	km.Unlock()
 }
 
-func (h *JwtHelper) GetKey(kid string) (k *Key, err error) {
+// GetKey() return the key struct by given kid.
+func GetKey(kid string) (k *Key, err error) {
 	if err := errorhelper.GenEmptyStringError(kid, "kid"); err != nil {
 		return nil, err
 	}
 
-	h.RLock()
-	k, ok := h.Keys[kid]
-	h.RUnlock()
+	km.RLock()
+	k, ok := km.Keys[kid]
+	km.RUnlock()
 
 	if !ok {
 		return nil, errorhelper.GenMapKeyNotFoundError(kid)
@@ -76,7 +76,16 @@ func (h *JwtHelper) GetKey(kid string) (k *Key, err error) {
 	return k, nil
 }
 
-func (h *JwtHelper) SetKeyFromFile(kid, alg, signKeyFile, verifyKeyFile string) (err error) {
+// SetKeyFromFile() reads the keys from the key files then stores the unique kid - Key struct pair.
+//
+//   Params:
+//       kid: Key id(unique).
+//       alg: JWT alg.
+//       signKeyFile: Signing key file.
+//       verifyKeyFile: Verifying key file.
+//   Return:
+//       err: error.
+func SetKeyFromFile(kid, alg, signKeyFile, verifyKeyFile string) (err error) {
 	key := &Key{}
 
 	if err := errorhelper.GenEmptyStringError(kid, "kid"); err != nil {
@@ -147,44 +156,71 @@ func (h *JwtHelper) SetKeyFromFile(kid, alg, signKeyFile, verifyKeyFile string) 
 		return errors.New(fmt.Sprintf("Incorrect alg: %s. Available algs: HS246,HS384,HS512,RS256,RS384,RS512,ES256,ES384,ES512", alg))
 	}
 
-	h.setKey(kid, key)
+	setKey(kid, key)
 	return nil
 }
 
-func (h *JwtHelper) SetHS256Key(kid, keyFile string) (err error) {
-	return h.SetKeyFromFile(kid, "HS256", keyFile, keyFile)
+// SetHS256Key() reads the HMAC SHA-256 key from key file and stores it with unique kid(key id).
+//
+// Key generation: github.com/northbright/keygen to generate HMAC SHA-XX key file.
+func SetHS256Key(kid, keyFile string) (err error) {
+	return SetKeyFromFile(kid, "HS256", keyFile, keyFile)
 }
 
-func (h *JwtHelper) SetHS384Key(kid, keyFile string) (err error) {
-	return h.SetKeyFromFile(kid, "HS384", keyFile, keyFile)
+// SetHS384Key() reads the HMAC SHA-384 key from key file and stores it with unique kid(key id).
+//
+// Key generation: github.com/northbright/keygen to generate HMAC SHA-XX key file.
+func SetHS384Key(kid, keyFile string) (err error) {
+	return SetKeyFromFile(kid, "HS384", keyFile, keyFile)
 }
 
-func (h *JwtHelper) SetHS512Key(kid, keyFile string) (err error) {
-	return h.SetKeyFromFile(kid, "HS512", keyFile, keyFile)
+// SetHS512Key() reads the HMAC SHA-512 key from key file and stores it with unique kid(key id).
+//
+// Key generation: github.com/northbright/keygen to generate HMAC SHA-XX key file.
+func SetHS512Key(kid, keyFile string) (err error) {
+	return SetKeyFromFile(kid, "HS512", keyFile, keyFile)
 }
 
-func (h *JwtHelper) SetRS256Key(kid, privKeyFile, pubKeyFile string) (err error) {
-	return h.SetKeyFromFile(kid, "RS256", privKeyFile, pubKeyFile)
+// SetRS256Key() reads the RSA SHA-256 keys from key files and stores it with unique kid(key id).
+//
+// Key generation: ssh-keygen -t rsa -b 2048
+func SetRS256Key(kid, privKeyFile, pubKeyFile string) (err error) {
+	return SetKeyFromFile(kid, "RS256", privKeyFile, pubKeyFile)
 }
 
-func (h *JwtHelper) SetRS384Key(kid, privKeyFile, pubKeyFile string) (err error) {
-	return h.SetKeyFromFile(kid, "RS384", privKeyFile, pubKeyFile)
+// SetRS384Key() reads the RSA SHA-384 keys from key files and stores it with unique kid(key id).
+//
+// Key generation: ssh-keygen -t rsa -b 2048
+func SetRS384Key(kid, privKeyFile, pubKeyFile string) (err error) {
+	return SetKeyFromFile(kid, "RS384", privKeyFile, pubKeyFile)
 }
 
-func (h *JwtHelper) SetRS512Key(kid, privKeyFile, pubKeyFile string) (err error) {
-	return h.SetKeyFromFile(kid, "RS512", privKeyFile, pubKeyFile)
+// SetRS512Key() reads the RSA SHA-512 keys from key files and stores it with unique kid(key id).
+//
+// Key generation: ssh-keygen -t rsa -b 2048
+func SetRS512Key(kid, privKeyFile, pubKeyFile string) (err error) {
+	return SetKeyFromFile(kid, "RS512", privKeyFile, pubKeyFile)
 }
 
-func (h *JwtHelper) SetES256Key(kid, privKeyFile, pubKeyFile string) (err error) {
-	return h.SetKeyFromFile(kid, "ES256", privKeyFile, pubKeyFile)
+// SetES256Key() reads the ECDSA(using P-256 curve and SHA-256) keys from key files and stores it with unique kid(key id).
+//
+// Key generation: ssh-keygen -t ecdsa -b 256
+func SetES256Key(kid, privKeyFile, pubKeyFile string) (err error) {
+	return SetKeyFromFile(kid, "ES256", privKeyFile, pubKeyFile)
 }
 
-func (h *JwtHelper) SetES384Key(kid, privKeyFile, pubKeyFile string) (err error) {
-	return h.SetKeyFromFile(kid, "ES384", privKeyFile, pubKeyFile)
+// SetES384Key() reads the ECDSA(using P-384 curve and SHA-384) keys from key files and stores it with unique kid(key id).
+//
+// Key generation: ssh-keygen -t ecdsa -b 384
+func SetES384Key(kid, privKeyFile, pubKeyFile string) (err error) {
+	return SetKeyFromFile(kid, "ES384", privKeyFile, pubKeyFile)
 }
 
-func (h *JwtHelper) SetES512Key(kid, privKeyFile, pubKeyFile string) (err error) {
-	return h.SetKeyFromFile(kid, "ES512", privKeyFile, pubKeyFile)
+// SetES512Key() reads the ECDSA(using P-521 curve and SHA-512) keys from key files and stores it with unique kid(key id).
+//
+// Key generation: ssh-keygen -t ecdsa -b 521(NOT 512)
+func SetES512Key(kid, privKeyFile, pubKeyFile string) (err error) {
+	return SetKeyFromFile(kid, "ES512", privKeyFile, pubKeyFile)
 }
 
 // CreateTokenString() creates a new JWT token string.
@@ -195,14 +231,14 @@ func (h *JwtHelper) SetES512Key(kid, privKeyFile, pubKeyFile string) (err error)
 //   Return:
 //       tokenString: new created JWT token string.
 //       err: error.
-func (h *JwtHelper) CreateTokenString(kid string, claims map[string]interface{}) (tokenString string, err error) {
+func CreateTokenString(kid string, claims map[string]interface{}) (tokenString string, err error) {
 	var k *Key
 
 	if err = errorhelper.GenEmptyStringError(kid, "kid"); err != nil {
 		return "", err
 	}
 
-	if k, err = h.GetKey(kid); err != nil {
+	if k, err = GetKey(kid); err != nil {
 		return "", err
 	}
 
@@ -210,6 +246,35 @@ func (h *JwtHelper) CreateTokenString(kid string, claims map[string]interface{})
 	t.Header["kid"] = kid
 	t.Claims = claims
 	return t.SignedString(k.SignKey)
+}
+
+// jwt-go's KeyFunc type:
+//
+// type Keyfunc func(*Token) (interface{}, error)
+func keyFunc(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Header["kid"]; !ok {
+		return nil, errorhelper.GenMapKeyNotFoundError("kid")
+	}
+
+	kid := ""
+	if str, ok := token.Header["kid"].(string); !ok {
+		msg := fmt.Sprintf("token.Header[\"kid\"]'s type is %T, but not string.", token.Header["kid"])
+		return nil, errors.New(msg)
+	} else {
+		kid = str
+	}
+
+	key, err := GetKey(kid)
+	if err != nil {
+		return nil, err
+	}
+
+	// Check signing method
+	if token.Method.Alg() != key.Method.Alg() {
+		return nil, errors.New("Signing Method Error.")
+	}
+
+	return key.VerifyKey, nil
 }
 
 // Parse() parses and validates the input token string.
@@ -221,36 +286,29 @@ func (h *JwtHelper) CreateTokenString(kid string, claims map[string]interface{})
 //       claims: map[string]interface{} to fill the jwt.Token[Claims].
 //       valid: token is valid or not.
 //       err: error.
-func (h *JwtHelper) Parse(tokenString string) (kid string, claims map[string]interface{}, valid bool, err error) {
-	t, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Header["kid"]; !ok {
-			return nil, errorhelper.GenMapKeyNotFoundError("kid")
-		}
-
-		kid = ""
-		if str, ok := token.Header["kid"].(string); !ok {
-			msg := fmt.Sprintf("token.Header[\"kid\"]'s type is %T, but not string.", token.Header["kid"])
-			return nil, errors.New(msg)
-		} else {
-			kid = str
-		}
-
-		key, err := h.GetKey(kid)
-		if err != nil {
-			return nil, err
-		}
-
-		// Check signing method
-		if token.Method.Alg() != key.Method.Alg() {
-			return nil, errors.New("Signing Method Error.")
-		}
-
-		return key.VerifyKey, nil
-	})
-
+func Parse(tokenString string) (kid string, claims map[string]interface{}, valid bool, err error) {
+	t, err := jwt.Parse(tokenString, keyFunc)
 	if err != nil {
 		return "", nil, false, err
 	}
 
-	return kid, t.Claims, t.Valid, nil
+	return t.Header["kid"].(string), t.Claims, t.Valid, nil
+}
+
+// ParseFromRequest() parses and validates the input token string in an http.Request. It's a wrapper of jwt.ParseFromRequest().
+//
+//   Params:
+//       r: http.Request may contain jwt token.
+//   Return:
+//       kid: Key id.
+//       claims: map[string]interface{} to fill the jwt.Token[Claims].
+//       valid: token is valid or not.
+//       err: error.
+func ParseFromRequest(r *http.Request) (kid string, claims map[string]interface{}, valid bool, err error) {
+	t, err := jwt.ParseFromRequest(r, keyFunc)
+	if err != nil {
+		return "", nil, false, err
+	}
+
+	return t.Header["kid"].(string), t.Claims, t.Valid, nil
 }
