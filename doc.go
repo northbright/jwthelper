e68fc68fc4427e636879e569dc8946675982f04a
Frank Xu <frank.xu@northbright.com>
2016-10-21 15:58:59 +0800

    Insert 'Package' before package description to remove golint warnings.

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
 /*
 
-jwthelper is a Golang package that provides JWT(JSON Web Token) functions based on jwt-go.
+Package jwthelper is a Golang package that provides JWT(JSON Web Token) functions based on jwt-go.
 
 */
 package jwthelper
