48b835b37d5864a4eebdad2c9f4484eaf78883f1
Frank Xu <frank.xu@northbright.com>
2017-12-28 13:57:11 +0800

    Export NewClaim() and remove other type specific functions

--- claims.go
+++ claims.go
@@ -29,8 +29,8 @@ func newClaims() claims {
 	}
 }
 
-// newClaim news a Claim and set the internal map with given name -> value pair.
-func newClaim(name string, value interface{}) Claim {
+// NewClaim news a Claim with given name -> value pair.
+func NewClaim(name string, value interface{}) Claim {
 	return Claim{func(c *claims) {
 		c.m.Lock()
 		defer c.m.Unlock()
@@ -38,53 +38,8 @@ func newClaim(name string, value interface{}) Claim {
 	}}
 }
 
-// StringClaim returns a Claim with string value.
-func StringClaim(name, value string) Claim {
-	return newClaim(name, value)
-}
-
-// IntClaim returns a Claim with int value.
-func IntClaim(name string, value int) Claim {
-	return newClaim(name, value)
-}
-
-// Int32Claim returns a Claim with int32 value.
-func Int32Claim(name string, value int32) Claim {
-	return newClaim(name, value)
-}
-
-// Int64Claim returns a Claim with int64 value.
-func Int64Claim(name string, value int64) Claim {
-	return newClaim(name, value)
-}
-
-// UintClaim returns a Claim with uint value.
-func UintClaim(name string, value uint) Claim {
-	return newClaim(name, value)
-}
-
-// Uint32Claim returns a Claim with uint32 value.
-func Uint32Claim(name string, value uint32) Claim {
-	return newClaim(name, value)
-}
-
-// Uint64Claim returns a Claim with uint64 value.
-func Uint64Claim(name string, value uint64) Claim {
-	return newClaim(name, value)
-}
-
-// Float32Claim returns a Claim with float32 value.
-func Float32Claim(name string, value float32) Claim {
-	return newClaim(name, value)
-}
-
-// Float64Claim returns a Claim with float64 value.
-func Float64Claim(name string, value float64) Claim {
-	return newClaim(name, value)
-}
-
 // TimeClaim returns a Claim with time.Time value.
 // It'll convert the time.Time to a Unix timestamp.
 func TimeClaim(name string, value time.Time) Claim {
-	return Int64Claim(name, value.Unix())
+	return NewClaim(name, value.Unix())
 }
