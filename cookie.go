dddf58ccbdd7b16ea78de2c06f0270e41bf5db32
Frank Xu <frank.xu@northbright.com>
2017-12-29 11:00:10 +0800

    Add NewCookie() to new a cookie contains JWT token

---
+++ cookie.go
@@ -0,0 +1,68 @@
+package jwthelper
+
+import (
+	"net/http"
+	"time"
+)
+
+type CookieOption struct {
+	f func(c *http.Cookie)
+}
+
+func CookieName(name string) CookieOption {
+	return CookieOption{func(c *http.Cookie) {
+		c.Name = name
+	}}
+}
+
+func CookiePath(path string) CookieOption {
+	return CookieOption{func(c *http.Cookie) {
+		c.Path = path
+	}}
+}
+
+func CookieDomain(domain string) CookieOption {
+	return CookieOption{func(c *http.Cookie) {
+		c.Domain = domain
+	}}
+}
+
+func CookieExpires(expires time.Time) CookieOption {
+	return CookieOption{func(c *http.Cookie) {
+		c.Expires = expires
+	}}
+}
+
+func CookieMaxAge(maxAge int) CookieOption {
+	return CookieOption{func(c *http.Cookie) {
+		c.MaxAge = maxAge
+	}}
+}
+
+func CookieSecure(secure bool) CookieOption {
+	return CookieOption{func(c *http.Cookie) {
+		c.Secure = secure
+	}}
+}
+
+func CookieHttpOnly(httpOnly bool) CookieOption {
+	return CookieOption{func(c *http.Cookie) {
+		c.HttpOnly = httpOnly
+	}}
+}
+
+func NewCookie(tokenString string, options ...CookieOption) http.Cookie {
+	cookie := http.Cookie{
+		Name:     "jwt",
+		Value:    tokenString,
+		Secure:   true,
+		HttpOnly: true,
+	}
+
+	// Override default cookie with customized options.
+	for _, op := range options {
+		op.f(&cookie)
+	}
+
+	return cookie
+}
